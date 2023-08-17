597a5175e166613e2ecf082939769cc0ff1c0274
mohansky <38560439+mohansky@users.noreply.github.com>
2023-08-17 15:41:56 +0530

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/style.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
